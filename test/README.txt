82b27428cc834fab091a95c394c2300518fb0f76
rocioimpa <rocio.impaglione@gmail.com>
2020-09-25 00:45:06 -0300

    WIP - app web para enviar forma backend

---
+++ test/README.txt
@@ -0,0 +1,10 @@
+1) Posicionarse en la carpeta y definir las siguientes variables:
+
+$ export FLASK_APP=main.py
+$ export FLASK_ENV=development
+
+2) Iniciar la app: 
+
+$ flask run
+
+3) Abrir en el browser http://localhost:5000. Por ahora el botón para redirigir al diagnóstico no funciona, así que entrar a /consulta para ver el formulario
---
+++ test/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/consulta', methods=('GET', 'POST'))
+def create():
+    return render_template('consulta.html')
